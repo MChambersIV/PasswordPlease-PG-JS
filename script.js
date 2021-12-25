e3bf5f6b35882c3dcf14bff82a5fdf3700bcfb0f
MChambersIV <chambersm025@gmail.com>
2021-12-25 15:06:31 -0800

    added intial parameters window prompt attached to generate password button and consequent rules to js

--- script.js
+++ script.js
@@ -11,18 +11,22 @@ function writePassword() {
 }
 
 var generator = {
-  upperCase: [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z],
-  lowerCase: [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z],
+  upperCase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+  lowerCase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
   numerals:  [0,1,2,3,4,5,6,7,8,9],
   specialCharacters: ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","}","|","~"],
 };
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); {
-  
+generateBtn.addEventListener("click", function() {
+  input = prompt("Please enter you desired password length. Note: It must be between 8 and 128 chracters in length","Ex: 8,20,100");
+    if (input>=8 && input<=128) {alert("Please continue");}
+     else {alert("Your password length is outside of the given parameters. Please try again.")
+
+  };
 
 
-}
+});
 
 //We want a password generator
     //When the password button on the html page is clicked a new random string of characters numbers and special characters will appear in the password box.
