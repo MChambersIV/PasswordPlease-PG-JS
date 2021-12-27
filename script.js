93535fa77689e3fe38facf3b74b007e590a00d53
MChambersIV <chambersm025@gmail.com>
2021-12-27 01:55:32 -0800

    renamed functions and renamed object containing password criteria indexes

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function writePassword() {
 
 };
 
-var generator = {
+var genArrays = {
   upperCase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
   lowerCase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
   numerals:  [0,1,2,3,4,5,6,7,8,9],
@@ -17,34 +17,44 @@ var generator = {
 };
 
 function stepOne() {
-  var inputOne = prompt("Please enter you desired password length. Note: It must be between 8 and 128 characters in length","Ex: 8,20,100");
-   if (inputOne>=8 && inputOne<=128) stepTwo();
+  var userInput = prompt("Please enter you desired password length. Note: It must be between 8 and 128 characters in length","Ex: 8,20,100");
+   if (userInput>=8 && userInput<=128) upperInput();
    else {alert("Your password length is outside of the given parameters. Please try again.");
     };
   };
 
-function stepTwo() {
-  var inputTwo = confirm("Would you like upper case characters in your password? Press cancel for no.");
-    if (inputTwo===true) stepThree();
-      else if (inputTwo===false) stepThree();
+function upperInput() {
+  var upperInput = confirm("Would you like upper case characters in your password? Press cancel for no.");
+    if (upperInput===true) lowerInput();
+      else if (upperInput===false) lowerInput();
 };
-function stepThree() {
-  var inputThree = confirm("Would you like lower case characters in your password? Press cancel for no.");
-    if (inputThree===true) stepFour();
-      else if (inputThree===false) stepFour();
+function lowerInput () {
+  var lowerInput = confirm("Would you like lower case characters in your password? Press cancel for no.");
+    if (lowerInput===true) numberInput();
+      else if (lowerInput===false) numberInput();
 };
-function stepFour() {
-  var inputFour= confirm("Would you like Numbers in your password? Press cancel for no.");
-    if (inputFour===true) stepFive()
-      else if (inputFour===false) stepFive();
+function numberInput() {
+  var numberInput= confirm("Would you like Numbers in your password? Press cancel for no.");
+    if (numberInput===true) specInput()
+      else if (numberInput===false) specInput();
 };
-function stepFive() {
-  var inputFive = confirm("Would you like special characters in your password? Press cancel for no.");
-    if (inputFive===true) genPass();
-      else if (inputFive===false) genPass();
+function specInput() {
+  var specInput = confirm("Would you like special characters in your password? Press cancel for no.");
+    if (specInput===true) genPass();
+      else if (specInput===false) genPass();
 };
 
 function genPass() {
+  if (stepTwo===true) {
+    for (var i=0; i<inputOne; i++) {
+     var i = generator.upperCase.sort(function(){return 0.5 - Math.random()});
+
+    };
+    
+  
+
+
+  };
  
 
 };
